3fe72b3e2308f1afe4705a14cbfb58a2567626b4
Andre Bossard <abossard@gmail.com>
2019-04-24 22:10:43 +0200

    ts 1

--- server.ts
+++ server.ts
@@ -1,12 +1,16 @@
 import * as fastify from "fastify"
 import {Http2Server, Http2ServerRequest, Http2ServerResponse } from "http2"
-
+import * as compress from "fastify-compress"
+import * as msgpack5 from "msgpack5"
 const generateMessage = () => ({
     ts: (new Date).getTime(),
     name: "Andre"
-})
+});
+
+const msgpack = msgpack5();
 
 const server: fastify.FastifyInstance<Http2Server, Http2ServerRequest, Http2ServerResponse> = fastify({})
+
 const opts: fastify.RouteShorthandOptions<Http2Server, Http2ServerRequest, Http2ServerResponse> = {
     schema: {
         response: {
@@ -20,11 +24,43 @@ const opts: fastify.RouteShorthandOptions<Http2Server, Http2ServerRequest, Http2
             }
         }
     }
-}
+};
 
-server.get('/ping', opts, (request, reply) => {
-    console.log(reply.res)
-    reply.code(200).send({pong: 'it worked'})
-})
+server.register(compress);
 
-process.exit(0)
+server.get('/generate', opts, (request, response) => {
+    const stream = msgpack.encoder();
+    let i = 0;
+    const max = 10000000;
+    const send = () => {
+        for (; i < max; i++) {
+            let success = stream.write(generateMessage());
+            if(!success) {
+                server.log.warn("Backpressure");
+                break;
+            }
+
+        }
+    };
+    send();
+    stream.pipe(response.res);
+    stream.on('drain', () => {
+        server.log.warn("Drain");
+        send();
+        if(i >= max -1) {
+            console.log("End");
+            stream.end();
+        }
+    });
+});
+
+const start = async () => {
+    try {
+        await server.listen(3000);
+        server.log.info(`server listening on ${server.server.address()}`)
+    } catch (err) {
+        server.log.error(err);
+        process.exit(1)
+    }
+};
+start();
